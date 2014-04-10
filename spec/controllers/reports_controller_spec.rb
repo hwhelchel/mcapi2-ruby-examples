ab2a5737b386131cf4f5c2a831f3bb358dd357fc
Harry Whelchel <harrywhelchel@gmail.com>
2014-04-10 16:35:29 -0700

    Adds tests for successful paths on reports controller.
    Adds tests for error messages on reports controller.

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -2,21 +2,85 @@ require 'spec_helper'
 
 describe ReportsController do
 
-  before(:each){ Mailchimp::Campaigns.any_instance.stub(:list){ raise Mailchimp::Error } }
+  let(:data) { ['abc'] }
+  let(:error_message) { Mailchimp::Error.new.message }
+  let(:params) { { id: 1 } }
+  let(:report) { 'report' }
   describe '#index' do
-    context 'when Mailchimp::Error is raised' do
-      it 'should redirect' do
+    context 'successful api call' do
+
+      before(:each) do
+        Mailchimp::Campaigns.any_instance.stub(:list){{'data' => data }}
         get :index
+      end
+
+      it 'renders the index template' do
+        expect(response).to render_template :index
+      end
+
+      it 'is ok' do
+        expect(response).to be_ok
+      end
+
+      it 'assigns campaigns' do
+        expect(assigns(:campaigns)).to eq data
+      end
+    end
+
+    context 'when Mailchimp::Error is raised' do
+      before(:each) do
+        Mailchimp::Campaigns.any_instance.stub(:list){ raise Mailchimp::Error }
+        get :index
+      end
+
+      it 'should redirect' do
         expect(response.status).to eq(302)
+      end
+
+      it 'should show appropriate error message' do
+        expect(request.flash[:error]).to eq error_message
       end
     end
   end
 
   describe '#show' do
+
+    context 'successful api call' do
+      before(:each) do
+        Mailchimp::Campaigns.any_instance.stub(:list){{'data' => data }}
+        Mailchimp::Reports.any_instance.stub(:summary){ report }
+        get :show, params
+      end
+
+      it 'renders the show template' do
+        expect(response).to render_template :show
+      end
+
+      it 'is ok' do
+        expect(response).to be_ok
+      end
+
+      it 'assigns campaign' do
+        expect(assigns(:campaign)).to eq data[0]
+      end
+
+      it 'assigns report' do
+        expect(assigns(:report)).to eq report
+      end
+    end
+
     context 'when Mailchimp::Error is raised' do
+      before(:each) do
+        Mailchimp::Campaigns.any_instance.stub(:list){ raise Mailchimp::Error }
+        get :show, params
+      end
+
       it 'should redirect' do
-        get :show, id: 1
         expect(response.status).to eq(302)
+      end
+
+      it 'should show appropriate error message' do
+        expect(request.flash[:error]).to eq error_message
       end
     end
   end
